b9dbad1a312b3cca9312bcf7ed57830e5c59ed06
owlks4 <115742200+owlks4@users.noreply.github.com>
2025-01-01 14:02:32 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dawn of the nth Day
+
+Generator for Majora's Mask-style 'Dawn of the First Day' text, but following the meme that you often see circulated at the beginning of the year, by declaring how many days are left in the year and the number of hours that remain.
---
+++ src/index.js
@@ -0,0 +1,144 @@
+let dawnOf = document.getElementById("dawn-of");
+let theDay = document.getElementById("the-first-day");
+let hoursRemain = document.getElementById("hours-remain");
+
+const items = [dawnOf,theDay,hoursRemain];
+
+let customDate = document.getElementById("custom-date");
+customDate.valueAsNumber = Date.now();
+
+let audio = document.getElementById("dawn-sound");
+
+theDay.onclick = ()=>{
+    updateDisplay(new Date(customDate.value), true);
+};
+
+document.getElementById("go").onclick = ()=>{updateDisplay(new Date(customDate.value), true)};
+
+const stringAliases = {
+    1:"one",
+    2:"two",
+    3:"three",
+    4:"four",
+    5:"five",
+    6:"six",
+    7:"seven",
+    8:"eight",
+    9:"nine",
+    10:"ten",
+    11:"eleven",
+    12:"twelve",
+    13:"thirteen",
+    14:"fourteen",
+    15:"fifteen",
+    20:"twenty",
+    30:"thirty",
+    40:"forty",
+    50:"fifty",
+    60:"sixty",
+    70:"seventy",
+    80:"eighty",
+    90:"ninety",
+}
+
+const concludingStrings = {
+    1:"first",
+    2:"second",
+    3:"third",
+    4:"fourth",
+    5:"fifth",
+    6:"sixth",
+    7:"seventh",
+    8:"eighth",
+    9:"ninth",
+    10:"tenth",
+    11:"eleventh",
+    12:"twelfth",
+    13:"thirteenth",
+    14:"fourteenth",
+    15:"fifteenth",
+    20:"twentieth",
+    30:"thirtieth",
+    40:"fortieth",
+    50:"fiftieth",
+    60:"sixtieth",
+    70:"seventieth",
+    80:"eightieth",
+    90:"ninetieth",
+}
+
+updateDisplay(new Date(), false);
+
+function updateDisplay(date, playSound){
+
+    let isLeapYear = new Date(date.getFullYear(), 1, 29).getMonth() === 1; //returns true if february 29th is a valid day this year
+
+    let day = getDayOfYearFromDate(date);
+    
+    let dayString = "the "+getAdjectiveString1To999(day)+" day";
+    theDay.innerHTML = dayString.replace(" and ","<br>and ");
+    document.title = "Dawn of "+ dayString;
+    
+    //theDay.innerText = "The "+getAdjectiveString1To999(2)+" Day";
+    
+    hoursRemain.innerText = "- "+ ((((isLeapYear ? 366 : 365) * 24) - (day * 24)) + 24) + " Hours Remain -";
+    
+    items.forEach(item => {
+        item.style.animation = 'none';
+        item.offsetHeight;
+        item.style.animation = null;
+    });
+
+    if (playSound){
+        audio.pause();
+        audio.currentTime = 0;
+        audio.play();
+    }
+}
+
+function getDaySuffix(day){
+    switch (day){
+        case 1:
+            return "st";
+        case 2:
+            return "nd";
+        case 3:
+            return "rd";
+        default:
+            return "th";
+    }
+}
+
+function getDayOfYearFromDate(dt) {
+    var current = new Date(dt.getTime());
+    var previous = new Date(dt.getFullYear(), 0, 1);
+    return Math.ceil((current - previous + 1) / 86400000);
+  }
+
+function getAdjectiveString1To999(input){
+    let output = "";
+    if (input > 99){        
+        output += stringAliases[Math.floor(input / 100)];
+        output += " hundred and "
+        while (input > 99){
+            input -= 100;
+        }
+    }
+    if (input > 15){
+        let tensColumn = Math.floor(input / 10);
+        while (input > 9){
+            input -= 10;
+        }
+        if (tensColumn == 1){
+            output += stringAliases[input]+"teenth"
+        } else if (input == 0) { //if there's nothing in the units column, so we need a concluding string like "fiftieth"
+            output += concludingStrings[tensColumn*10];
+        } else {
+            output += stringAliases[tensColumn*10] +"-"+ concludingStrings[input];
+        }
+    } else {
+        output += concludingStrings[input];
+    }
+
+    return output;
+}
